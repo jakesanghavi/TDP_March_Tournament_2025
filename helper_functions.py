06476e489fa39ae0dad12a2f8be340af519f232b
Jake Sanghavi <57878447+jakesanghavi@users.noreply.github.com>
2025-03-11 09:37:31 -0400

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -67,7 +67,7 @@ def simulate_tournament_sklearn(test_data, model):
             break
 
         # Get our predictors and the teams by row
-        round_X = round_data.drop(columns=['RegionTeamA', 'RegionTeamB', 'TeamA', 'TeamB', 'ResultTeamA'])
+        round_X = round_data.drop(columns=['RegionTeamA', 'RegionTeamB', 'TeamA', 'TeamB', 'ResultTeamA', 'ScoreTeamA', 'ScoreTeamB'], errors='ignore')
         round_teams = round_data[['TeamA', 'TeamB', 'RegionTeamA', 'RegionTeamB', 'SeedTeamA', 'SeedTeamB']]
                 
         # Predict winners using the model you made
@@ -101,9 +101,9 @@ def simulate_tournament_sklearn(test_data, model):
                 
                 # Get stats for each team
                 team_A_stats = test_data[(test_data['TeamA'] == next_team_A) | (test_data['TeamB'] == next_team_A)].\
-                    drop(columns=['TeamA', 'TeamB', 'ResultTeamA', 'SeedTeamA', 'SeedTeamB']).iloc[-1]
+                    drop(columns=['TeamA', 'TeamB', 'ResultTeamA', 'SeedTeamA', 'SeedTeamB', 'ScoreTeamA', 'ScoreTeamB']).iloc[-1]
                 team_B_stats = test_data[(test_data['TeamA'] == next_team_B) | (test_data['TeamB'] == next_team_B)].\
-                    drop(columns=['TeamA', 'TeamB', 'ResultTeamA', 'SeedTeamA', 'SeedTeamB']).iloc[-1]
+                    drop(columns=['TeamA', 'TeamB', 'ResultTeamA', 'SeedTeamA', 'SeedTeamB', 'ScoreTeamA', 'ScoreTeamB']).iloc[-1]
 
             
                 # Get only relevant columns
@@ -165,7 +165,7 @@ def simulate_tournament_pytorch(test_data, model, device='cpu'):
         if round_data.empty:
             break
 
-        round_X = round_data.drop(columns=['RegionTeamA', 'RegionTeamB', 'TeamA', 'TeamB', 'ResultTeamA'])
+        round_X = round_data.drop(columns=['RegionTeamA', 'RegionTeamB', 'TeamA', 'TeamB', 'ResultTeamA', 'ScoreTeamA', 'ScoreTeamB'], errors='ignore')
         round_teams = round_data[['TeamA', 'TeamB', 'RegionTeamA', 'RegionTeamB', 'SeedTeamA', 'SeedTeamB']]
         
         with torch.no_grad():
@@ -198,9 +198,9 @@ def simulate_tournament_pytorch(test_data, model, device='cpu'):
                 next_team_B, seed_team_B = winners[i + 1][0], winners[i + 1][2]
                 
                 team_A_stats = test_data[(test_data['TeamA'] == next_team_A) | (test_data['TeamB'] == next_team_A)].\
-                    drop(columns=['TeamA', 'TeamB', 'ResultTeamA', 'SeedTeamA', 'SeedTeamB']).iloc[-1]
+                    drop(columns=['TeamA', 'TeamB', 'ResultTeamA', 'SeedTeamA', 'SeedTeamB', 'ScoreTeamA', 'ScoreTeamB']).iloc[-1]
                 team_B_stats = test_data[(test_data['TeamA'] == next_team_B) | (test_data['TeamB'] == next_team_B)].\
-                    drop(columns=['TeamA', 'TeamB', 'ResultTeamA', 'SeedTeamA', 'SeedTeamB']).iloc[-1]
+                    drop(columns=['TeamA', 'TeamB', 'ResultTeamA', 'SeedTeamA', 'SeedTeamB', 'ScoreTeamA', 'ScoreTeamB']).iloc[-1]
                 
                 team_A_stats = team_A_stats[team_A_stats.index.str.contains('TeamA') | team_A_stats.index.isin(extra_cols_a)]
                 team_B_stats = team_B_stats[team_B_stats.index.str.contains('TeamB') | team_B_stats.index.isin(extra_cols_b)]
